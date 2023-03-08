3f157e05714dad4193509ba4d6097634fce47446
Carlos Enrique Gutiérrez Juárez <75094293+carlos28d@users.noreply.github.com>
2023-03-07 19:00:54 -0600

    Changed domain in redirect

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://heartfelt-taiyaki-6082f9.netlify.app/";
 let accessToken;
 const Spotify = {
   getAcessToken() {
